b19336efb8ce74f32b9585fd3060a71a2a5cfdc8
Tian-Liang Huang <tlhuang.tw@gmail.com>
2024-06-23 13:55:40 +0800

    made logger global

--- main.go
+++ main.go
@@ -10,24 +10,30 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+var Logger *slog.Logger
+
 func main() {
-	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
-	logger.Info("logger launched")
+	Logger = slog.New(slog.NewTextHandler(os.Stdout, nil))
+	Logger.Info("logger launched")
 
 	storage.StorageMgr = storage.NewStorageInstance()
 	storage.StorageMgr.SetTaskPoolSize(100) // PoolSize should be from dockerfile
 	storage.StorageMgr.InitTaskIDPool()
+	Logger.Info("storage is ready")
 
+	Logger.Info("the server is going to run")
 	svr := initRouter()
 	svr.Run(":8000")
 
 	// we might need gracefully shutdown
 	// need to deal with panic?
 	// custom errors?
+	Logger.Info("the server has been shutdown")
 }
 
 func initRouter() *gin.Engine {
 	router := gin.Default()
+	Logger.Info("start registering API routes")
 
 	// health check
 	router.GET("/healthCheck", api.HealthCheck)
@@ -38,5 +44,6 @@ func initRouter() *gin.Engine {
 	router.PUT("/tasks/:id", api.UpdateTask)
 	router.DELETE("/tasks/:id", api.DeleteTask)
 
+	Logger.Info("routes were registered successfully")
 	return router
 }
